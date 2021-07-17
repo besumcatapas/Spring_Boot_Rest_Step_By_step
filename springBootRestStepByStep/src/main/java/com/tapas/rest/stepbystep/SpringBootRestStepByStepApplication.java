f422cf95621cef68abd4ca2b659db5863877d6a1
Tapas <84491685+besumcatapas@users.noreply.github.com>
2021-07-17 09:20:09 +0530

    Add files via upload
    initial project code load

---
+++ springBootRestStepByStep/src/main/java/com/tapas/rest/stepbystep/SpringBootRestStepByStepApplication.java
@@ -0,0 +1,13 @@
+package com.tapas.rest.stepbystep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestStepByStepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestStepByStepApplication.class, args);
+	}
+
+}
---
+++ springBootRestStepByStep/src/test/java/com/tapas/rest/stepbystep/SpringBootRestStepByStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapas.rest.stepbystep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestStepByStepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
